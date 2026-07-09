e2b9b1a72f24b99d491ed9146bea79237a6f3ad1
iamjpescobar <iamjpescobar@gmail.com>
2026-07-08 21:43:59 -0400

    Add type hints to build_danger_zone function
    Updated the build_danger_zone function signature to include type hints for batter_profile and the return type.

--- engines/engines/danger_zone.py
+++ engines/engines/danger_zone.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def build_danger_zone(batter_profile):
+def build_danger_zone(batter_profile: dict) -> pd.DataFrame:
     """
     Builds a 3x3 danger grid from batter profile:
     High/Mid/Low x Inside/Middle/Outside
