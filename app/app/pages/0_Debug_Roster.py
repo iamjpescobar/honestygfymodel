c6981c4bb4b06f5314798dcf0df6d68032b52482
iamjpescobar <iamjpescobar@gmail.com>
2026-07-13 00:14:03 +0000

    Remove stray duplicate app folder

--- app/app/pages/0_Debug_Roster.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from engines.roster import get_live_team_roster
-
-team = st.text_input("Team name:", "Toronto Blue Jays")
-
-if st.button("Test Roster"):
-    roster = get_live_team_roster(team)
-    st.write(roster)
-
